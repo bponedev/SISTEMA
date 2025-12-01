caae7bed497278d1b1889245dfeec9f0acd8717f
bponedev <bponedev@gmail.com>
2025-12-01 10:30:25 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from app import create_app
-
-# Arquivo principal que inicia a aplicação Flask
-# Mantido extremamente simples para evitar bugs.
-
-app = create_app()
+from app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
