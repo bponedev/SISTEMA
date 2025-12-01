e4482a2c64af9a1fc4a7ad868ac55aa32b3d6f6f
bponedev <bponedev@gmail.com>
2025-12-01 09:59:16 -0300

    Create main.js

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+// ==========================================================
+// Arquivo principal JS do sistema
+// Funções simples e independentes
+// ==========================================================
+
+console.log("Sistema iniciado com sucesso.");
