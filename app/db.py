a1f84b9c23154d2a33a0ae7c5a02b653077e1a29
bponedev <bponedev@gmail.com>
2025-12-02 10:34:20 -0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -5,7 +5,13 @@ def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
             current_app.config["DB_PATH"],
-            detect_types=sqlite3.PARSE_DECLTYPES
+            detect_types=sqlite3.PARSE_DECLTYPES,
+            check_same_thread=False
         )
         g.db.row_factory = sqlite3.Row
     return g.db
+
+def init_db():
+    db = get_db()
+    with current_app.open_resource("schema.sql") as f:
+        db.executescript(f.read().decode("utf-8"))
